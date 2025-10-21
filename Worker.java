02df3c8f10a4efce5f8f62658fe588919f643855
Nato0012 <melodynathana@gmail.com>
2025-10-21 18:10:40 +0900

    Update Worker.java

--- Worker.java
+++ Worker.java
@@ -5,7 +5,7 @@ public class Worker {
   private int morale;
 
   public Worker(){
-    this("None",0.0,0.5,100)
+    this("None",0.0,0.5,100);
   }
   public Worker(String name, double experience, double efficiency, int morale){
     this.name = name;
