f73d8872a6b6327308cc6557f6570aa6dfacfe4f
Nato0012 <melodynathana@gmail.com>
2025-10-21 18:09:05 +0900

    Create Worker.java

---
+++ Worker.java
@@ -0,0 +1,16 @@
+public class Worker {
+  private String name;
+  private double experience;
+  private double efficiency;
+  private int morale;
+
+  public Worker(){
+    this("None",0.0,0.5,100)
+  }
+  public Worker(String name, double experience, double efficiency, int morale){
+    this.name = name;
+    this.experience = experience;
+    this.efficiency = efficiency;
+    this.morale = morale;
+  }
+  
